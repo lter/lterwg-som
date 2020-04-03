870640a58603262f96d720cb5d5928e09142cea9
wwieder <wwieder@ucar.edu>
2020-04-03 11:04:42 -0600

    Initial plots for overview paper

---
+++ data-projects/Overview_paper/QuickSOM_dataCoverage.R
@@ -0,0 +1,68 @@
+# QuickSOM_dataCoverage"
+# Wil Wieder
+# Built on work from Derek Pierson & Jessica Moore
+# April 3, 2020
+
+rm(list=ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+print(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+data.all <- readRDS("/Users/wwieder/Will/git_repos_local/lterwg-som/somCompositeData_2020-02-11.rds")  
+
+data.all$full_name <- paste0(data.all$site_code," ",data.all$location_name)  #Concatenate strings
+data.all <- data.all %>% mutate(full_name = gsub("NA", "", full_name))  #Remove NA from strings
+
+#get first four characters from observation_date
+#first, make sure all are characters 
+data.all$observation_date <- str_replace(data.all$observation_date, "Aug-Sep 2011", "2011")
+data.all$year<-substr(data.all$observation_date, start=1, stop=4)
+data.all$year_all<-as.numeric(data.all$year)
+
+#check for unique 4 digit values in year
+print(unique(data.all$year_all))
+
+#-------------------------------------------
+## SOC Percents
+### Clean up SOC percent values
+#filter out negative soc%
+data.soc.percent<-data.all %>% 
+  filter(!is.na(lyr_soc)) %>% filter(lyr_soc>0) %>% filter(lyr_soc<100)
+
+soc.per<-ggplot(data.soc.percent %>% filter(network!="none"), 
+                aes(x=site_code, y=as.numeric(year)), na.omit=T)+
+  geom_point(aes(color=network), show.legend = FALSE) +
+  #  geom_point() +
+  xlab("Site") + ylab("Year") +
+  scale_y_continuous(breaks=seq(1975,2019,5),na.value=2020)+
+  scale_x_discrete(labels = abbreviate) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=8)) +
+  facet_grid(.~network, scales = "free_x", space='free_x', drop=FALSE)
+
+soc.per
+
+soc.dep<-ggplot(data.all %>% filter(network!="none"), 
+                aes(x=site_code, y=layer_bot), na.omit=T)+
+  geom_point(aes(color=network), show.legend = FALSE, size=1) +
+  #  geom_point() +
+  xlab("Site") + ylab("Depth") +
+  scale_y_reverse() +
+  scale_x_discrete(labels = abbreviate) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size=8)) +
+  facet_grid(.~network, scales = "free_x", space='free_x', drop=FALSE) 
+
+soc.dep
+
+pdf(file = "fig4.pdf",   # The directory you want to save the file in
+    width = 15, # The width of the plot in inches
+    height = 3) # The height of the plot in inches
+
+soc.per
+dev.next()
+
+soc.dep
+dev.off()
+
+
---
+++ data-projects/Overview_paper/SOM_data_mapper.R
@@ -0,0 +1,86 @@
+#Script to map SOM tarball data
+#Derek Pierson, piersond@oregonstate.edu
+#Created: 8/28/2019
+
+rm(list=ls())
+library(dplyr)
+library(tidyverse)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#get tarball
+tarball <- readRDS("/Users/wwieder/Will/git_repos_local/lterwg-som/somCompositeData_2020-02-11.rds")  
+tarball <- tarball %>% filter(google_dir != "NA")
+
+#Select the data columns reqd
+map.df <- tarball %>% select(c("site_code","network","lat", "long","lyr_soc"))
+
+#make sure coordinates are numeric
+map.df$lat <- as.numeric(map.df$lat)
+map.df$long <- as.numeric(map.df$long)
+
+#strip tarball of all data without coordinates
+map.df <- map.df %>% 
+  filter(lat != "NA") %>% 
+  filter (long != "NA") %>% 
+  filter(network!="none")
+
+#round the coordinates to two decimal places
+  # 1 decimal place is ~10 km resolution
+  # 2 decimal places is ~1 km resolution
+  # 3 decimal places is ~100 m resolution
+map.df <- map.df %>% mutate_at(vars(long,lat), round, 1)
+
+#remove duplicate values by creating unified lat-long column and removing duplicates
+simplmap.df <- map.df 
+simplmap.df$latlong <- paste0(map.df$lat,map.df$long) 
+simplmap.df <- simplmap.df[!duplicated(simplmap.df$latlong),]
+
+###########################
+##Simple point map
+library(maps)
+
+map('world')
+points(simplmap.df$long, simplmap.df$lat, col=2, pch=19)
+
+
+########################################
+#ggplot data by Network map
+
+#World map
+mapWorld <- borders("world", colour="gray40", fill="white") 
+mapWorld <- ggplot() + mapWorld + 
+  geom_point(data = simplmap.df, aes(x=long, y = lat, color=network), 
+             size=3, alpha=0.5, show.legend = FALSE) + #position=position_jitter(h=0.7,w=0.7)) + 
+   ylim(-53,80) +
+  coord_fixed(1.3) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank())
+
+mapWorld 
+
+#USA map
+mapUSA <- borders("usa", colour="gray40", fill="white") 
+mapUSA <- ggplot()  + mapUSA + 
+  geom_point(data = simplmap.df, aes(x=long, y = lat, color=network), 
+             size=3, alpha=0.6) + #position=position_jitter(h=0.7,w=0.7)) + 
+  borders("state") +
+  xlim(-125,-67) + ylim(25,50) +
+  coord_fixed(1.3) +
+  scale_size_area() +
+  coord_quickmap() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank())
+
+mapUSA
+
+pdf(file = "fig3.pdf",   # The directory you want to save the file in
+    width = 6, # The width of the plot in inches
+    height = 4) # The height of the plot in inches
+
+mapWorld
+dev.next()
+
+mapUSA
+dev.off()
