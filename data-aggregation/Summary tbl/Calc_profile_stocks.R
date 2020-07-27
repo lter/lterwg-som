d786b5a1ea969a1dac3ece0aa6223b1a38e3e5ab
Unknown <drk.pierson@gmail.com>
2020-07-27 14:14:55 -0700

    Update to profile analyte avg calc script
    Nothing major added, remains work in progress. See script header for the "To Do" list.

--- data-aggregation/Summary tbl/Calc_profile_stocks.R
+++ data-aggregation/Summary tbl/Calc_profile_stocks.R
@@ -2,6 +2,28 @@
 # More details @ https://github.com/lter/lterwg-som/issues/72
 # Created: July 2020, Derek Pierson
 
+
+# TO DO as of 7/27/20
+#######################################################################################
+# 1) Implement max depth parameter
+# 2) Better handling of locs withonly one layer
+# 3) Add a top layer only option to ftn
+# 4) Add a stock option which uses bulk density
+
+# A) Find why some sites have overlapping increments, add column to differentiate
+# B) Remove -1 to 0 cm layers from profile calc, add to separate column in output
+# C) Decide if filling gaps is worth doing
+
+# NEXT NEW FTN: Build a ftn for locational averages of stocks
+#   - Sequential cluster avgs to a specified separation distance
+#   - Or avg by close proximity and similar elevation (pull elevation by lat long from USGS)
+#   - Plot/map results to look for errors
+
+# LOOKING AHEAD: Can ftns also get us sand, silt, clay?
+#   - Or pull in USGS soil classification values by lat long
+
+#######################################################################################
+
 # Load libraries
 library(tidyverse)
 
@@ -41,40 +63,55 @@ analyte_location_summary <- function(database, analyte) {
 ### Test function output
 tbl1 <-   analyte_location_summary(database = som, analyte = "lyr_soc")#pre-function implementation values
 
-#Create dataframe for stocks
+#Create blank dataframe to rbind stocks from stocks_by_loc function
 stocks_df <- data.frame(site_code=character(),
                         location_name=character(), 
                         latlong=character(),
                         gaps = numeric(),
                         depth = numeric(),
                         analyte_profile_conc = numeric(),
+                        calc_notes = character(),
                         stringsAsFactors=FALSE) 
 
 #Function to calc analyte stock for a single location
 stocks_by_loc <- function(location, analyte_data, max_depth) {
   
   #DEBUG
-  #location = "63.880708, -149.239569"
+  #location = "44.95597, -110.54196"
   #analyte_data = tbl1
-  #max_depth = 50
+  #max_depth = 300
     
   #Filter data by location
   df <- analyte_data %>% filter(latlong == location) %>% arrange(layer_top)
   
   if(nrow(df) < 2) {
-    error = 1
-    #print("Only one value, skipped")
+    stock_calc_error = "Single depth increment"
+    #output <- data.frame(site_code=unique(df$site_code),
+    #                     location_name=unique(df$location_name), 
+    #                     latlong=unique(df$latlong),
+    #                     gaps = NA,
+    #                     depth = df$layer_bot[nrow(df)],
+    #                     analyte_profile_conc = df$analyte_avg[nrow(df)],
+    #                     calc_notes = stock_calc_error,
+    #                     stringsAsFactors=FALSE)
+    
   } else if(any(duplicated(df$layer_top))) {
-    error = 2
-    #print("Overlapping layers error")
+    stock_calc_error = "Mixed depth increments"
+    #output <- data.frame(site_code=unique(df$site_code),
+    #                     location_name=unique(df$location_name), 
+    #                     latlong=unique(df$latlong),
+    #                     gaps = NA,
+    #                     depth = df$layer_bot[nrow(df)],
+    #                     analyte_profile_conc = NA,
+    #                     calc_notes = stock_calc_error,
+    #                     stringsAsFactors=FALSE)
   } else {  
     
-    #for error tracking
+    ###for error tracking
     #print(df$latlong)
     
     #filter depth to below max
     df <- df %>% filter(layer_top < max_depth)
-    
     
     #find gaps in depth increments
     #Create empty gaps list
@@ -107,22 +144,41 @@ stocks_by_loc <- function(location, analyte_data, max_depth) {
                          gaps = num_of_gaps,
                          depth = bot_depth,
                          analyte_profile_conc = analyte_prof_conc,
+                         calc_notes = " ",
                          stringsAsFactors=FALSE)
+    
+    #return values as dataframe with 1 row
     return(output)
   }
+  
+  
+  
 }
 
 #test the function for one location
-single_loc_test <- stocks_by_loc(location = "63.880708, -149.239569",
+single_loc_test2 <- stocks_by_loc(location = "44.968096, -110.492027",   #63.880708, -149.239569
                       analyte_data = tbl1, 
-                      max_depth = 50)
+                      max_depth = 300)
 
 #Run functiion across all locations and rbind output to dataframe
 get_loc_stocks <- sapply(unique(tbl1$latlong), stocks_by_loc, analyte_data = tbl1, max_depth=500)
 stocks <- do.call(rbind, get_loc_stocks)
 
+
+str(get_loc_stocks)
+
+#Remove the lat-long row names
+rownames(stocks) <- c()
+
+
+#QA gaps, how many locs with depth increment gaps?
+qa_df <- stocks %>% mutate(gaps = as.numeric(gaps)) %>% filter(gaps > 0)
+
+
+
+
 #save stocks tbl
-write.csv(stocks, "lyr-soc_stocks_output.csv")
+#write.csv(stocks, "lyr-soc_stocks_output.csv")
 
 
 
